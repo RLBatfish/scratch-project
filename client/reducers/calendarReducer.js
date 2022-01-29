0bb348dd5fc139faafbe16a12418119ba0245064
Jessica <jlouielee@gmail.com>
2022-01-29 14:24:01 -0800

    Installed Redux DevTools Extension. Updated reducers: calendarReducer, index.

---
+++ client/reducers/calendarReducer.js
@@ -0,0 +1,18 @@
+import * as types from '../constant/actionTypes.js';
+
+const initialState = {
+    month: '',
+    days: [],
+};
+
+const calendarReducer = (state = initialState, action) => {
+
+    switch (action.types) {
+     
+        
+
+
+
+        
+    }
+};
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,2 +1,10 @@
 import { combineReducers } from 'redux';
 import calendarReducer from './calendarReducer'
+
+const reducers = combineReducers({
+
+    calendar: calendarReducer,
+
+});
+
+export default reducers;
