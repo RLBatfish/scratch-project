432f2651a62cc0fb20976187ab28f327e5c1904f
Eric Gomez <ergomez0201@gmail.com>
2022-01-29 10:21:56 -0800

    added entry to front-end and server listening on port 3000

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
+app.get('/', (req, res, err) => {
+  if (err) {
+    console.log('there is an error');
+  }
+  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
