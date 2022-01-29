8cd092ea8cc33120b14b1c0be9347542c76331e8
Jessica <jlouielee@gmail.com>
2022-01-29 14:18:09 -0800

    Added client/style/_style.scss, src/index.js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from '../client/App.jsx';
+import store from './store';
+import style from '../client/style/application.scss';
+
 
 render(<App />, document.getElementById('root'));
