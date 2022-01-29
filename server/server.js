13f4d0fc20a5a666c0b8ed1ad35e6e85c668e900
Jessica <jlouielee@gmail.com>
2022-01-29 15:45:34 -0800

    Commented express static in server.js.

--- server/server.js
+++ server/server.js
@@ -3,15 +3,19 @@ const path = require('path');
 
 const app = express();
 
+//global middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
 
-app.use('/dist', express.static(path.join(__dirname, '../dist')));
+//app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res, err) => {
   if (err) {
     console.log('there is an error');
   }
-  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+  return res.status(200).send('Hello from the server!');
 });
 
 
